79a4006a5356189a3992150526cff091b41943d8
Thiago Araújo <thiagoaraujoadvg@gmail.com>
2025-11-22 15:37:48 -0300

    first commit

---
+++ patient-service/src/test/java/com/ojuara/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ojuara.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
